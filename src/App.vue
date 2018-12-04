650d44bf2f00f4ea7c0cc2230e22071b637707fb
Juan Sifontez <juansfontz@gmail.com>
2018-12-03 22:59:20 -0400

    Add modal component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <modal />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Modal
   }
 }
 </script>
 
 <style>
+body, html{
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+}
+.modal__overlay{
+  background: rgba(0, 0, 0, .8);
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal{
+  width: 650px;
+  background-color: white;
+  padding: 2em;
+  border-radius: 1.5em;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.modal__buttons{
+  display: flex;
+  justify-content: space-around;
+}
+.btn{
+  text-align: center;
+  padding: .6em .8em .8em;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  color: white;
+  background-color: lightgray;
+  outline: 0;
+  font-size: 24px;
+  transition: .2s;
+}
+.btn:active{
+  transform: scale(.9);
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="modal__overlay">
+    <div class="modal">
+      <div class="modal__buttons">
+        <button class="btn">Open One Thing</button>
+        <button class="btn">Open Anoher one</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'modal'
+}
+</script>
