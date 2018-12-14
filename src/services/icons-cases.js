b63dd3f0547d660a6ff49d88797412343f794aab
Juan Sifontez <juansfontz@gmail.com>
2018-12-13 21:39:06 -0400

    Add icons cases object lookups

---
+++ src/services/icons-cases.js
@@ -0,0 +1,22 @@
+const iconsCases = {
+  '01d': './assets/clear_sky_sun.svg',
+  '01n': './assets/clear_sky_moon.svg',
+  '02d': './assets/few_clouds_sun.svg',
+  '02n': './assets/few_clouds_moon.svg',
+  '03d': './assets/scattered_clouds.svg',
+  '03n': './assets/scattered_clouds.svg',
+  '04d': './assets/broken_coulds.svg',
+  '04n': './assets/broken_coulds.svg',
+  '09d': './assets/shower_rain.svg',
+  '09n': './assets/shower_rain.svg',
+  '10d': './assets/rain_sun.svg',
+  '10n': './assets/rain_moon.svg',
+  '11d': './assets/thunderstorm.svg',
+  '11n': './assets/thunderstorm.svg',
+  '13d': './assets/snow.svg',
+  '13n': './assets/snow.svg',
+  '50d': 'https://openweathermap.org/img/w/50d.png',
+  '50n': 'https://openweathermap.org/img/w/50d.png'
+}
+
+export default iconsCases
