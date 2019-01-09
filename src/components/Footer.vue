b7564e88a6bd01e5dcffd64d048f52cdefb1c87d
Juan Sifontez <juansfontz@gmail.com>
2019-01-09 18:59:26 -0400

    Add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="footer">
+    <p>Powered by <a href="https://openweathermap.org" target="_blank">OpenWeaterMap</a></p>
+    <p>Made with <span role="img" aria-label="heart">❤ </span> <a href="https://twitter.com/Jsifontez_" target="_blank" rel="noopener noreferrer">@Jsifontez_</a></p>
+    <p>Commits are welcome on <a href="http://github.com/100DaysOfVue/show-local-weather" target="_blank" rel="noopener noreferrer">Github</a></p>
+  </div>
+</template >
