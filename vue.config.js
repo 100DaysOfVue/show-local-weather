373bcc5da69d169c3a02e3fa53352711f9d7535d
Juan Sifontez <juansfontz@gmail.com>
2019-01-14 20:13:18 -0400

    Add new base url

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
-    ? 'https://github.com/100DaysOfVue/show-local-weather/tree/master/dist/'
+    ? 'show-local-weather'
     : '/',
   indexPath: '../index.html'
 }
